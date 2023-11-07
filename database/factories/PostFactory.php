3c3dcf5fee23c4f4605090cd0b113696a1474cf1
Nico Deblauwe <nico@bluepundit.eu>
2023-11-07 12:07:01 +0100

    FEATURE :sparkles: Add Post model to application

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'title' => fake()->sentence(5, true),
+            'body' => fake()->paragraphs(3, true),
+            'is_flagged' => fake()->boolean(2),
+            'published_at' => fake()->optional()->dateTimeBetween('-1 year', 'now'),
+            'user_id' => fake()->numberBetween(1, 10),
+            'category_id' => fake()->numberBetween(1, 5),
+        ];
+    }
+}
