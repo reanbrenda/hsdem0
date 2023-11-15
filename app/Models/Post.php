c3eccf84affb5df8331c7b33961efa026e413000
Nico Deblauwe <nico@bluepundit.eu>
2023-11-15 13:00:14 +0100

    FEATURE :sparkles: Add slug to posts models

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,10 +4,19 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
 
 class Post extends Model
 {
     use HasFactory;
+
+    // Model events -----------------------------------------------------------
+    protected static function booted()
+    {
+        static::creating(function ($post) {
+            $post->slug = $post->slug ?? Str::slug($post->title);
+        });
+    }
 
     // Model Relationships -----------------------------------------------------
     public function author()
