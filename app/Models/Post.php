a85904d582976b0ed5dc42ca547d7ee38396e56a
Nico Deblauwe <nico@bluepundit.eu>
2023-11-16 12:43:17 +0100

    FEATURE :sparkles: Add image to post model (and it's factory)

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,10 +5,17 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
+use Spatie\Image\Manipulations;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
+use Spatie\MediaLibrary\MediaCollections\Models\Media;
 
-class Post extends Model
+class Post extends Model implements HasMedia
 {
     use HasFactory;
+    use InteractsWithMedia;
+
+    protected $guarded = [];
 
     protected $casts = [
         'published_at' => 'datetime',
@@ -31,5 +38,19 @@ class Post extends Model
     public function categories()
     {
         return $this->belongsToMany(Category::class);
+    }
+
+    // Medialibrary settings ----------------------------------------------------------
+    public function registerMediaConversions(Media $media = null): void
+    {
+        $this
+            ->addMediaConversion('preview')
+            ->fit(Manipulations::FIT_CROP, 300, 200)
+            ->nonQueued();
+        $this
+            ->addMediaConversion('thumbnail')
+            ->fit(Manipulations::FIT_CROP, 50, 50)
+            ->nonQueued();
+
     }
 }
--- database/factories/PostFactory.php
+++ database/factories/PostFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Post;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
 /**
@@ -23,5 +24,15 @@ class PostFactory extends Factory
             'published_at' => fake()->optional()->dateTimeBetween('-1 year', 'now'),
             'author_id' => fake()->numberBetween(1, 10),
         ];
+    }
+
+    public function configure()
+    {
+         return $this->afterCreating(function (Post $post) {
+            $url = 'https://source.unsplash.com/random/1200x800';
+            $post
+                ->addMediaFromUrl($url)
+                ->toMediaCollection();
+        });
     }
 }
