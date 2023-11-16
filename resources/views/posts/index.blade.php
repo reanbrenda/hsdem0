95301cc9920c6a4bafee5d5da5951f7495ee3ab4
Nico Deblauwe <nico@bluepundit.eu>
2023-11-16 16:20:26 +0100

    ART :art: Making layout prettier

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -7,28 +7,35 @@
     <ul class="grid grid-cols-2 gap-4">
     @foreach($posts as $post)
 
-        <li class="block p-2 shadow-sm bg-white hover:shadow-lg rounded-lg">
+        <li class="block shadow-sm bg-white hover:shadow-lg rounded-lg grid grid-cols-3 group overflow-hidden">
 
-            <img src="{{$post->media->first()->getUrl('preview')}}">
+            <a href="{{route('posts.show', ['post' => $post])}}" class="block h-full bg-red-50 rounded-l overflow-hidden">
+                <img src="{{$post->media->first()->getUrl('preview')}}" class="rounded-l h-full group-hover:scale-110 transition duration-500 cursor-pointer object-cover">
+            </a>
 
-            <div class="flex gap-x-2 mb-2">
-                @foreach($post->categories as $category)
-                    <a href="{{route('categories.show', ['id' => $category->id])}}" class="bg-teal-500 hover:bg-teal-600 text-white rounded-full py-1 px-4 text-xs">
-                        {{$category->name}}
-                    </a>
-                @endforeach
+            <div class="col-span-2 p-2">
+                <div class="flex gap-x-2 mb-2">
+                    @foreach($post->categories as $category)
+                        <a href="{{route('categories.show', ['id' => $category->id])}}" class="uppercase bg-teal-500 hover:bg-teal-600 text-white rounded-full py-1 px-4 text-xs">
+                            {{$category->name}}
+                        </a>
+                    @endforeach
+                </div>
+
+                <a href="{{route('posts.show', ['post' => $post])}}" class="">
+                    <h2 class="font-bold text-lg" >{{$post->title}}</h2>
+                    <span class="">{{$post->author?->name ?? 'UNKNOWN AUTHOR' }}</span>
+                    <span class="text-teal-500 mx-1">|</span>
+                    <span>{{$post->published_at->format('Y-m-d H:i')}}</span>
+
+                    <div class="px-1 pt-2 line-clamp-3">
+                        {{ Str::limit($post->body, 200) }}
+                    </div>
+                </a>
+
+
             </div>
 
-            <a href="{{route('posts.show', ['post' => $post])}}" class="">
-                <h2 class="font-bold text-lg" >{{$post->title}}</h2>
-                <span class="">{{$post->author?->name ?? 'UNKNOWN AUTHOR' }}</span>
-                <span class="text-teal-500 mx-1">|</span>
-                <span>{{$post->published_at->format('Y-m-d H:i')}}</span>
-
-                <div class="px-1 pt-2">
-                    {{ Str::limit($post->body, 200) }}
-                </div>
-            </a>
         </li>
     @endforeach
     </ul>
