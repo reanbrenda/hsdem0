36e4001612190822aed135fae0cb86a3986cca3c
Nico Deblauwe <nico@bluepundit.eu>
2023-11-17 18:03:55 +0100

    FEATURE :sparkles: Add post management to navbar

--- app/View/Components/SiteLayoutNavbar.php
+++ app/View/Components/SiteLayoutNavbar.php
@@ -9,6 +9,7 @@ use Illuminate\View\Component;
 class SiteLayoutNavbar extends Component
 {
     public array $menu_items;
+    public array $home_menu_items;
     public array $admin_menu_items;
 
     public function __construct()
@@ -18,6 +19,10 @@ class SiteLayoutNavbar extends Component
             ['label' => 'Posts', 'route' => 'posts.index', 'url' => null],
             ['label' => 'Categories', 'route' => 'categories.index', 'url' => null],
             ['label' => 'Authors', 'route' => 'users.index', 'url' => null],
+        ];
+
+        $this->home_menu_items = [
+            ['label' => 'My posts', 'route' => 'home.posts.index', 'url' => null],
         ];
 
         $this->admin_menu_items = [
--- resources/views/components/site-layout-navbar.blade.php
+++ resources/views/components/site-layout-navbar.blade.php
@@ -64,6 +64,18 @@
                         -->
                         <div x-show="admin_menu_visible" class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
 
+                            @auth
+                                <span class="block px-4 py-2 text-xs uppercase text-gray-400 font-semibold">My data</span>
+                                @foreach($home_menu_items as $item)
+                                    @if(request()->routeIs($item['route']))
+                                        <a href="{!! $item['url'] ?? route($item['route']) !!}" class="block px-4 pt-2 text-sm bg-gray-100 text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">{{$item['label']}}</a>
+                                    @else
+                                        <a href="{!! $item['url'] ?? route($item['route']) !!}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">{{$item['label']}}</a>
+                                    @endif
+                                @endforeach
+                                <hr class="mt-2">
+                            @endauth
+
                             @if( auth()->user()->is_admin)
                                 <span class="block px-4 py-2 text-xs uppercase text-gray-400 font-semibold">Manage data</span>
                                 @foreach($admin_menu_items as $item)
