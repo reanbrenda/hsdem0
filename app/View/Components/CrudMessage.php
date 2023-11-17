ba45e04ac205d2c229c931f50197289e1af0f6e3
Nico Deblauwe <nico@bluepundit.eu>
2023-11-17 17:56:26 +0100

    Refactor :hammer: Make crud message component more universal

--- app/View/Components/CrudMessage.php
+++ app/View/Components/CrudMessage.php
@@ -6,7 +6,7 @@ use Closure;
 use Illuminate\Contracts\View\View;
 use Illuminate\View\Component;
 
-class CrudSuccessMessage extends Component
+class CrudMessage extends Component
 {
     /**
      * Create a new component instance.
@@ -21,6 +21,6 @@ class CrudSuccessMessage extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.crud-success-message');
+        return view('components.crud-message');
     }
 }
--- resources/views/components/crud-message.blade.php
+++ resources/views/components/crud-message.blade.php
@@ -3,3 +3,9 @@
         {{ session()->get('success_notification') }}
     </div>
 @endif
+
+@if(session()->has('error_notification'))
+    <div class="p-2 bg-red-50 border border-red-200 text-red-500 mb-4 rounded">
+        {{ session()->get('error_notification') }}
+    </div>
+@endif
