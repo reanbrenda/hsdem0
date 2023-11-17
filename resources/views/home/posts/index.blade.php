4acba82828247978ae95f7539b164cfcebd1baef
Nico Deblauwe <nico@bluepundit.eu>
2023-11-17 18:50:19 +0100

    DESIGN :art: Make author  posts index more attractive

--- resources/views/home/posts/index.blade.php
+++ resources/views/home/posts/index.blade.php
@@ -1,23 +1,56 @@
 <x-site-layout title="USER ZONE - MY POSTS">
-    <a class="underline" href="{{route('home.posts.create')}}">Create a post</a>
-
-    <hr/><br/>
 
     <x-crud-message/>
 
-    <ul>
-        @foreach($posts as $post)
-            <li>
-                {{$post->title}} -
-                <a class="underline" href="{{route('home.posts.show', $post)}}">show</a>
-                <a class="underline" href="{{route('home.posts.edit', $post)}}">edit</a>
-                @if($post->published_at === null)
-                    <a class="underline" href="{{route('home.posts.toggle-publish', $post)}}">publish</a>
-                @else
-                    <a class="underline" href="{{route('home.posts.toggle-publish', $post)}}">unpublish</a>
-                @endif
-            </li>
-        @endforeach
-    </ul>
+    <div class="px-4 sm:px-6 lg:px-8">
+        <div class="sm:flex sm:items-center">
+            <div class="sm:flex-auto">
+                <h1 class="text-base font-semibold leading-6 text-gray-900">My posts</h1>
+                <p class="mt-2 text-sm text-gray-700">A list of all the posts you wrote.</p>
+            </div>
+            <div class="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+                <a href="{{route('home.posts.create')}}" class="block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Write a new post</a>
+            </div>
+        </div>
+        <div class="mt-8 flow-root">
+            <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div class="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+                    <div class="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+                        <table class="min-w-full divide-y divide-gray-300">
+                            <thead class="bg-gray-50">
+                            <tr>
+                                <th scope="col" class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6">Title</th>
+                                <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900">Publication status</th>
+                                <th scope="col" class="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                                    <span class="sr-only">Actions</span>
+                                </th>
+                            </tr>
+                            </thead>
+                            <tbody class="divide-y divide-gray-200 bg-white">
+                            @foreach($posts as $post)
+                            <tr>
+                                <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                                    {{$post->title}}</td>
+                                <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                                    {{$post->published_at ?? 'UNPUBLISHED DRAFT'}}
+                                    @if($post->published_at === null)
+                                        <a class="font-medium  text-green-600 hover:text-green-900" href="{{route('home.posts.toggle-publish', $post)}}">publish</a>
+                                    @else
+                                        <a class="font-medium text-red-200 hover:text-red-900" href="{{route('home.posts.toggle-publish', $post)}}">unpublish</a>
+                                    @endif
+                                </td>
+                                <td class="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                                    <a href="{{route('home.posts.edit', $post)}}" class="text-indigo-600 hover:text-indigo-900">Edit<span class="sr-only">, Lindsay Walton</span></a>
+                                </td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
 
 </x-site-layout>
