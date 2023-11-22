267fd7b4e7591ef6ed169b0e39bc80e4483f9f06
Nico Deblauwe <nico@bluepundit.eu>
2023-11-22 13:59:27 +0100

    FEATURE :sparkles: Improve tests

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -22,3 +22,15 @@ test('posts show page is being displayed', function () {
     // Assert
     $response->assertStatus(200);
 });
+
+test('posts show page contains title, body and author info', function () {
+    // Arrange
+    $author = User::factory()->create();
+    $post = Post::factory()->create(['author_id' => 1]);
+
+    // Act
+    $response = $this->get('/posts/' . $post->slug);
+
+    // Assert
+    $response->assertSeeInOrder([$post->title, $post->author->name, $post->body]);
+});
--- tests/Feature/WelcomeTest.php
+++ tests/Feature/WelcomeTest.php
@@ -10,3 +10,37 @@ test('welcome page is being displayed', function () {
     $response->assertStatus(200);
 });
 
+
+test('4 most recent articles are shown', function () {
+    // Arrange
+    $posts = Post::factory(5)->withoutAfterCreating()->published()
+        ->create(['is_featured' => false])
+        ->sortByDesc(fn($p) => $p->published_at);
+
+    // Act
+    $response = $this->get('/');
+
+    // Assert - only first 4 published posts are shown in correct order, 5th is not shown
+    $response->assertSeeInOrder($posts->take(4)->pluck('title')->toArray());
+    $response->assertDontSee($posts->skip(4)->take(1)->first()->title);
+});
+
+
+test('4 most recent articles are shown (and no unpublished ones)', function () {
+    // Arrange
+    $posts = Post::factory(5)->withoutAfterCreating()->published()
+        ->create(['is_featured' => false])
+        ->sortByDesc(fn($p) => $p->published_at);
+
+    $posts = $posts->map( function($p) use($posts) {
+        if($p == $posts->first()) {$p->update(['published_at' => null]);}
+        return $p;
+    });
+
+    // Act
+    $response = $this->get('/');
+
+    // Assert that (different!) 4 most recent articles are shown
+    $response->assertSeeInOrder($posts->skip(1)->take(4)->pluck('title')->toArray());
+    $response->assertDontSee($posts->first()->title);
+});
