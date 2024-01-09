e8a703e28eeae13ddd497422f85c3af644465eea
Nico Deblauwe <nico@bluepundit.eu>
2024-01-09 12:08:11 +0100

    CONTENT :pencil: Change the navbar looks

--- resources/views/layouts/site-navbar.blade.php
+++ resources/views/layouts/site-navbar.blade.php
@@ -11,7 +11,7 @@
 
                         @foreach($menu_items as $item)
                             @if(request()->routeIs($item['route']))
-                                <a href="{!! $item['url'] ?? route($item['route']) !!}" class="bg-red-900 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">{{$item['label']}}</a>
+                                <a href="{!! $item['url'] ?? route($item['route']) !!}" class="bg-gray-600 text-white rounded-md px-3 py-2 text-sm font-medium" aria-current="page">{{$item['label']}}</a>
                             @else
                                 <a href="{!! $item['url'] ?? route($item['route']) !!}" class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">{{$item['label']}}</a>
                             @endif
